efa51ab3f920fc99472b43f8a62e32f77fbfb568
VÖRÖSKŐI András <voroskoi@gmail.com>
2024-12-22 21:06:23 +0100

    Empty Tardy initialization

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,19 @@
 const std = @import("std");
+const Tardy = @import("tardy").Tardy(.auto);
+const Runtime = @import("tardy").Runtime;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var t = try Tardy.init(.{ .allocator = std.heap.page_allocator });
+    defer t.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    try t.entry(
+        {},
+        struct {
+            fn init(_: *Runtime, _: void) !void {}
+        }.init,
+        {},
+        struct {
+            fn deinit(_: *Runtime, _: void) !void {}
+        }.deinit,
+    );
 }
