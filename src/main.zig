719a53c1841ec2899cad7fcb00b8e55dcbbc96d3
VÖRÖSKŐI András <voroskoi@gmail.com>
2024-12-22 21:42:05 +0100

    first try

--- src/main.zig
+++ src/main.zig
@@ -6,10 +6,31 @@ pub fn main() !void {
     var t = try Tardy.init(.{ .allocator = std.heap.page_allocator });
     defer t.deinit();
 
+    // XXX: this stack is not visible in init: add to init params!
+    // var stack = std.ArrayList(usize).init(std.heap.page_allocator);
+    // defer stack.deinit();
+
+    // XXX: This struct fails with unable to unwrap null on utils.zig:27
+    // because it neads to be `packed` to have a backing_integer field.
+    // OTOH if I add packed it becomes u128 which fails on utils.zig:71.
+    const StackContext = struct {
+        stack: *std.ArrayList(usize),
+        item: usize,
+
+        fn append(_: *Runtime, _: void, context: @This()) !void {
+            try context.stack.append(context.item);
+        }
+    };
+
     try t.entry(
         {},
         struct {
-            fn init(_: *Runtime, _: void) !void {}
+            fn init(rt: *Runtime, _: void) !void {
+                var stack = std.ArrayList(usize).init(std.heap.page_allocator);
+                defer stack.deinit();
+
+                try rt.spawn(void, StackContext{ .stack = &stack, .item = 3 }, StackContext.append);
+            }
         }.init,
         {},
         struct {
